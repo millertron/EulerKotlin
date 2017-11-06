243b850a86d91f44b90aaf4dffa1f02860849bd7
millertronv <senjinkoha@gmail.com>
2017-11-06 10:56:43 +0000

    Main program takes user input to select problem to solve.

--- src/EulerMain.kt
+++ src/EulerMain.kt
@@ -1,3 +1,12 @@
+import java.util.*
+
 fun main(args: Array<String>) {
+    print("Select Project Euler problem number:")
+    with(Scanner(System.`in`)) {
+        when(nextInt()){
+            1 -> print(multiplesOfThree())
+            else -> print("Option unavailable")
+        }
+    }
     print(multiplesOfThree())
 }
